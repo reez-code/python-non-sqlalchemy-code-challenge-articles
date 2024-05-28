3836c505f26e76b228f00e2e6779e1dcda5c19ae
Phil <phil.github@gmail.com>
2024-05-28 21:39:52 +0300

    half done

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -4,9 +4,46 @@ class Article:
         self.magazine = magazine
         self.title = title
         
+    @property 
+    def title(self):
+        return self._title
+    
+    @title.setter
+    def title(self, title):
+        if(hasattr(self, "title")):
+            print("Already Exists")
+        else:
+            if isinstance(title, str) and 5 <= len(title) <= 50:
+                self._title = title
+    
+    @property
+    def author(self):
+        return self._author
+    
+    @author.setter
+    def author(self, author):
+        if isinstance(author, Author):
+            self._author = author
+
+
+
+
 class Author:
     def __init__(self, name):
         self.name = name
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        if(hasattr(self, "name")):
+            print("Already Exists")
+        else:
+             if isinstance(name, str) and len(name) > 0:
+                  self._name = name
+
 
     def articles(self):
         pass
@@ -24,6 +61,24 @@ class Magazine:
     def __init__(self, name, category):
         self.name = name
         self.category = category
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and 2 <= len(name) <= 16:
+            self._name = name
+    
+    @property
+    def category(self):
+        return self._category
+    
+    @category.setter
+    def category(self, category):
+        if isinstance(category, str) and len(category) > 0:
+            self._category = category
 
     def articles(self):
         pass
@@ -36,3 +91,7 @@ class Magazine:
 
     def contributing_authors(self):
         pass
+
+   
+    
+   
